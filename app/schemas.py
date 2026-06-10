227cdce3022fbf52327d7127c936f34bcdfea1dd
Ram77-7 <ramrohith2002bu@gmail.com>
2026-06-10 11:26:16 +0530

    Delete app directory

--- app/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel 
-
-class loan_input(BaseModel):
-    Gender : str 
-    Married : str 
-    Education : str 
-    Self_Employed : str 
-    Applicant_Income : int 
-    Property_Area : str
